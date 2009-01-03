f53fa042fdb278e1d4dc82701f3cc0c2282a9541
Aaron Blohowiak <aaronblohowiak@host-187-4-0-10.sfoffice.verticalresponse.com>
2009-01-03 00:25:47 -0800

    updating git_wiki for finding pages by name

--- lib/git_wiki.rb
+++ lib/git_wiki.rb
@@ -14,10 +14,39 @@ class GitWiki
   end
   
   def [](page_name)
-    Page.new( repo, page_name )
+    page = find_or_create_page(page_name)
+  end
+
+  def []=(page_name, contents)
+    page = find_or_create_page(page_name)
+    page.contents = contents
+    page.save
   end
   
-  private
+  def find_or_create_page(page_name)
+    find_page(page_name) || Page.new( self, page_name )
+  end
+  
+  def find_page(page_name)
+    dirpath = File.dirname(page_name)
+    dir = if dirpath == '.' 
+            repo.tree 
+          else
+            repo.tree / dirpath
+          end
+    return nil unless dir
+
+    filename = File.basename(page_name)+'.*'
+    blob = dir.contents.detect do |item|
+             File.fnmatch(filename, item.name)
+           end
+    return nil unless blob 
+    
+    ext = File.extname(blob.name)
+    Page.new(self, page_name, {:ext => ext, :content => blob.data })
+  end
+  
+private
   
   def find_or_create_repo
     Grit::Repo.new(@path)
